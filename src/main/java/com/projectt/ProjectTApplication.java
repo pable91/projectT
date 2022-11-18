b5544b2d0418bad00b58968b114b3300135a567b
김용권 <dydrnjs518@cnspartner.com>
2022-11-18 16:37:59 +0900

    Initial commit

---
+++ src/main/java/com/projectt/ProjectTApplication.java
@@ -0,0 +1,13 @@
+package com.projectt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectTApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectt/ProjectTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

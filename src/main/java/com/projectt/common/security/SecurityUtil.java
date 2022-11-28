a01e6fa4be0c21d8ec4c2aade2a5e7e2008e3c57
김용권 <dydrnjs518@cnspartner.com>
2022-11-28 16:40:44 +0900

    refactor: 유틸리티 클래스 객체 생성 제한

--- src/main/java/com/projectt/common/security/SecurityUtil.java
+++ src/main/java/com/projectt/common/security/SecurityUtil.java
@@ -1,6 +1,8 @@
 package com.projectt.common.security;
 
 import com.projectt.domain.model.User;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -8,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SecurityUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityUtil.class);

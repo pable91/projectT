3735f45674f810e5279fe6d69b20149512185602
김용권 <dydrnjs518@cnspartner.com>
2022-12-06 10:29:07 +0900

    test: mock을 활용한 controller 테스트 코드 작성

--- src/main/java/com/projectt/domain/dto/request/SignupUserDto.java
+++ src/main/java/com/projectt/domain/dto/request/SignupUserDto.java
@@ -4,25 +4,27 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class SignupUserDto {
 
     @ApiModelProperty(value = "유저 아이디", example = "userId1", required = true)
     @NotBlank
-    private final String userid;
+    private String userid;
 
     @ApiModelProperty(value = "패스워드", example = "1234", required = true)
     @NotBlank
-    private final String pw;
+    private String pw;
 
     @ApiModelProperty(value = "유저 이름", example = "kim", required = true)
     @NotBlank
-    private final String username;
+    private String username;
 
     @Override
     public String toString() {
